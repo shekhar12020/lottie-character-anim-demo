f8e08114b491a4b2daa3df971f6ec5c3f8d3db64
Shekhar Sahu <shekhar@Shekhars-MacBook-Pro.local>
2021-03-17 15:29:02 +0530

    TTS option added. Ui modifcation done.

--- app/src/main/java/com/demo/lottie_character_anim/MainActivity.kt
+++ app/src/main/java/com/demo/lottie_character_anim/MainActivity.kt
@@ -1,30 +1,27 @@
 package com.demo.lottie_character_anim
 
 import android.animation.Animator
-import android.animation.ValueAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.speech.tts.TextToSpeech
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+
+    var tts: TextToSpeech? = null
     val rules: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        0 - 30 -------- U
-//                31 - 60 -------- T,H
-//        61 - 90 -------- Q,W
-//        91 - 120-------- O
-//                121- 150-------- L
-//                151- 180-------- F,V
-//        181- 210-------- C,H,J,S,H
-//        211- 240-------- C,D,G,K,R,N,S
-//        241- 270-------- B,M,P
-//        271- 300-------- AEI
+        tts = TextToSpeech(this, this)
 
         rules.add("U/0-30")
         rules.add("T,H/31-60")
@@ -37,47 +34,49 @@ class MainActivity : AppCompatActivity() {
         rules.add("B,M,P/241-270")
         rules.add("A,E,I/271-300")
 
-        val wordToSpell = "olalaleooooo"
-        findViewById<TextView>(R.id.wordToSpellText).text = wordToSpell
-
         val talkAnimation: LottieAnimationView = findViewById(R.id.talkAnimation);
-        talkAnimation.speed = 4.3f
+        talkAnimation.speed = 6f
         talkAnimation.imageAssetsFolder = "images/"
         talkAnimation.setAnimation("character_anim.json")
 
-        val lock: Any = Any()
+        val wordToSpell = findViewById<TextView>(R.id.textInputBox).text
 
-        var charPosition = 0
+        findViewById<TextView>(R.id.speakButton).setOnClickListener {
 
-        var range = getRange(wordToSpell[charPosition++])
-        talkAnimation.setMinAndMaxFrame(range.split("-")[0].toInt(), range.split("-")[1].toInt())
-        talkAnimation.addAnimatorListener((object : Animator.AnimatorListener {
-            override fun onAnimationStart(anim: Animator?) {
-            }
+            tts?.speak(wordToSpell, TextToSpeech.QUEUE_FLUSH, null, null)
 
-            override fun onAnimationEnd(anim: Animator?) {
-                talkAnimation.removeAllAnimatorListeners()
-                if (charPosition < wordToSpell.length) {
-                    talkAnimation.addAnimatorListener(this)
-                    range = getRange(wordToSpell[charPosition++])
-                    talkAnimation.setMinAndMaxFrame(range.split("-")[0].toInt(), range.split("-")[1].toInt())
-                    talkAnimation.playAnimation()
-                } else {
-                    charPosition = 0
-                    talkAnimation.addAnimatorListener(this)
-                    var range = getRange(wordToSpell[charPosition++])
-                    talkAnimation.setMinAndMaxFrame(range.split("-")[0].toInt(), range.split("-")[1].toInt())
-                    talkAnimation.playAnimation()
+            var charPosition = 0
+
+            var range = getRange(wordToSpell[charPosition++])
+            talkAnimation.setMinAndMaxFrame(range.split("-")[0].toInt(), range.split("-")[1].toInt())
+            talkAnimation.addAnimatorListener((object : Animator.AnimatorListener {
+                override fun onAnimationStart(anim: Animator?) {
                 }
-            }
 
-            override fun onAnimationRepeat(anim: Animator?) {
-            }
+                override fun onAnimationEnd(anim: Animator?) {
+                    talkAnimation.removeAllAnimatorListeners()
+                    if (charPosition < wordToSpell.length) {
+                        talkAnimation.addAnimatorListener(this)
+                        range = getRange(wordToSpell[charPosition++])
+                        talkAnimation.setMinAndMaxFrame(range.split("-")[0].toInt(), range.split("-")[1].toInt())
+                        talkAnimation.playAnimation()
+//                } else {
+//                    charPosition = 0
+//                    talkAnimation.addAnimatorListener(this)
+//                    range = getRange(wordToSpell[charPosition++])
+//                    talkAnimation.setMinAndMaxFrame(range.split("-")[0].toInt(), range.split("-")[1].toInt())
+//                    talkAnimation.playAnimation()
+                    }
+                }
 
-            override fun onAnimationCancel(anim: Animator?) {
-            }
-        }))
-        talkAnimation.playAnimation()
+                override fun onAnimationRepeat(anim: Animator?) {
+                }
+
+                override fun onAnimationCancel(anim: Animator?) {
+                }
+            }))
+            talkAnimation.playAnimation()
+        }
     }
 
     private fun getRange(char: Char): String {
@@ -87,5 +86,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return "241-270"
+    }
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts?.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(applicationContext, "Language not supported", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(applicationContext, "Init failed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
